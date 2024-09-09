b7970dec1390ffa77116c49378a981342d21df7e
samruddhi4041 <kinderapply20@gmail.com>
2024-09-09 18:07:28 +0530

    Update Footer.jsx

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ import { useState } from "react";
 const Footer = () => {
   const [message, setMessage] = useState();
   const sendMessage = () => {
-    window.location = `https://wa.me/8291590374?text=${message}`;
+    window.location = `https://wa.me/8779362797?text=${message}`;
   };
   return (
     <>
@@ -33,7 +33,7 @@ const Footer = () => {
                 <Link>
                   <i
                     onClick={() => {
-                      window.location = `https://wa.me/8291590374?text=Say Hello..!!`;
+                      window.location = `https://wa.me/8779362797?text=Say Hello..!!`;
                     }}
                   >
                     {<IoLogoWhatsapp />}
@@ -60,7 +60,7 @@ const Footer = () => {
                 <i>
                   <MdPhoneForwarded />
                 </i>
-                +91 8291590374
+                +91 8779362797
               </Link>
 
               <a href="mailto:samruddhi4040@gmail.com" className="links">
